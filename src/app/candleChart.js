1fdb847ce8eafd5895247d297ac7a3cc29410a94
Divyansh Agarwal <77184239+nikkuAg@users.noreply.github.com>
2024-11-01 00:54:07 +0530

    Update candleChart.js

--- src/app/candleChart.js
+++ src/app/candleChart.js
@@ -1,12 +1,16 @@
-import CanvasJSReact from "@canvasjs/react-charts"
+"use client"
+import { useState, useEffect } from "react"
 
-var CanvasJSChart = CanvasJSReact.CanvasJSChart
 export const CandleChart = ({ data }) => {
-	const dataPoints = data.map((d) => ({
-		x: new Date(d.date),
-		y: [d.open, d.high, d.low, d.close],
-	}))
-	console.log(dataPoints)
+	const [CanvasJSChart, setCanvasJSChart] = useState(null)
+
+	useEffect(() => {
+		const loadCanvasJSChart = async () => {
+			const CanvasJSReact = await import("@canvasjs/react-charts")
+			setCanvasJSChart(() => CanvasJSReact.default.CanvasJSChart)
+		}
+		loadCanvasJSChart()
+	}, [])
 	const options = {
 		theme: "light3",
 		animationEnabled: false,
@@ -43,13 +47,16 @@ export const CandleChart = ({ data }) => {
 				type: "candlestick",
 				color: "#707070",
 				fillOpacity: 0.7,
-				risingColor: "red",
-				fallingColor: "green",
+				risingColor: "green",
+				fallingColor: "red",
 				yValueFormatString: "0.000",
 				xValueFormatString: "MMM DD, YYYY",
-				dataPoints: dataPoints,
+				dataPoints: data.map((d) => ({
+					x: new Date(d.date),
+					y: [d.open, d.high, d.low, d.close],
+				})),
 			},
 		],
 	}
-	return <CanvasJSChart options={options} />
+	return CanvasJSChart ? <CanvasJSChart options={options} /> : <>Loading</>
 }
